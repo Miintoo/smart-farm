93eefd9efba090c95af46fcf2de68a1cdfe39820
yujin-kwak <yujin02371@gmail.com>
2023-05-01 15:19:25 +0900

    feat: Sensor 상태 정보 구현

---
+++ src/components/info/SensorStatus.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+function sensorStatus() {
+  return (
+    <Status>
+      <StatusImage alt="좋음상태" src="images/smile.png" />
+      <StatusMessage>좋음</StatusMessage>
+    </Status>
+  );
+}
+
+const Status = styled.div`
+  display: flex;
+  align-items: center;
+  border: 2px solid #c6a692;
+  background: #ffffff;
+  border-radius: 10px;
+  margin-left: 3%;
+  height: 100%;
+  width: 100%;
+`;
+
+const StatusImage = styled.img`
+  margin-left: 3%;
+`;
+
+const StatusMessage = styled.p`
+  font-family: 'Jua';
+  font-size: 2.25rem;
+  line-height: 2rem;
+  color: #c6a692;
+  margin: 0.4% 0 0 2%;
+`;
+
+export default sensorStatus;
--- src/pages/info/DHTInfo.jsx
+++ src/pages/info/DHTInfo.jsx
@@ -1,13 +1,48 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import SensorMenu from '../../components/info/SensorMenu';
+import SensorStatus from '../../components/info/SensorStatus';
 
 function DHTInfo() {
+  const deviceName = '상추';
   return (
     <>
       <SensorMenu />
-      <p>온습도 페이지</p>
+      <Wrapper>
+        <DeviceName>디바이스: {deviceName}</DeviceName>
+        <SensorInfoWrapper>
+          <SensorStatus />
+        </SensorInfoWrapper>
+      </Wrapper>
     </>
   );
 }
+
+const Wrapper = styled.div`
+  width: 90vw;
+  height: 85vh;
+  margin: 0 auto;
+  background: #f0e7e2;
+  border-radius: 8px;
+`;
+
+const DeviceName = styled.p`
+  position: relative;
+  width: 100%;
+  font-family: 'Jua';
+  font-size: 1.5rem;
+  line-height: 1.875rem;
+  color: #c6a692;
+  padding-top: 2%;
+  margin-left: 3%;
+`;
+
+const SensorInfoWrapper = styled.div`
+  position: absolute;
+  display: flex;
+  margin-top: 1.5%;
+  width: 87%;
+  height: 12%;
+`;
 
 export default DHTInfo;
