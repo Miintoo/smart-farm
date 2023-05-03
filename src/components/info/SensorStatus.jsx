1533ee343748802d2cb65266849603c16062ec4d
yujin-kwak <yujin02371@gmail.com>
2023-05-03 15:39:27 +0900

    Design: 온습도 페이지 10px -> 1rem 기준으로 변경

--- src/components/info/SensorStatus.jsx
+++ src/components/info/SensorStatus.jsx
@@ -14,27 +14,26 @@ const Status = styled.div`
   display: flex;
   align-items: center;
   box-sizing: border-box;
-  border: 0.125rem solid #c6a692;
-  border-radius: 0.625rem;
+  border: 0.2rem solid #c6a692;
+  border-radius: 1rem;
   width: 100%;
   height: 100%;
-  margin-left: 3%;
 
   background: #ffffff;
 `;
 
 const StatusImage = styled.img`
-  margin-left: 5%;
+  margin-left: 3.6rem;
 `;
 
 const StatusMessage = styled.p`
-  margin: 0.4% 0 0 3%;
+  margin: 0.3rem 0 0 2.5rem;
 
   color: #c6a692;
 
   font-family: 'Jua';
-  font-size: 2.25rem;
-  line-height: 2rem;
+  font-size: 3.6rem;
+  line-height: 4.5rem;
 `;
 
 export default sensorStatus;
--- src/pages/info/DHTInfo.jsx
+++ src/pages/info/DHTInfo.jsx
@@ -3,7 +3,7 @@ import styled from '@emotion/styled';
 import SensorMenu from '../../components/info/SensorMenu';
 import SensorStatus from '../../components/info/SensorStatus';
 
-function DHTInfo() {
+export default function DHTInfo() {
   const deviceName = '상추';
   return (
     <>
@@ -19,35 +19,33 @@ function DHTInfo() {
 }
 
 const Wrapper = styled.div`
+  position: relative;
   width: 90vw;
   height: 85vh;
-  border-radius: 0.5rem;
+  border-radius: 1rem;
   margin: 0 auto;
 
   background: #f0e7e2;
 `;
 
 const DeviceName = styled.p`
-  position: relative;
+  position: absolute;
 
   width: 100%;
-  padding-top: 2%;
-  margin-left: 3%;
+  margin: 1.7rem 0 0 2.6rem;
 
   color: #c6a692;
 
   font-family: 'Jua';
-  font-size: 1.5rem;
-  line-height: 1.875rem;
+  font-size: 2.4rem;
+  line-height: 3rem;
 `;
 
 const SensorInfoWrapper = styled.div`
   position: absolute;
 
   display: flex;
-  width: 87%;
-  height: 12%;
-  margin-top: 1.5%;
+  width: calc(100% - 4.8rem);
+  height: 10rem;
+  margin: 5.6rem 2.4rem;
 `;
-
-export default DHTInfo;
