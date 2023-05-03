36eed783440bd07d7d607639da0f3579d84a8644
yujin-kwak <yujin02371@gmail.com>
2023-05-03 15:54:15 +0900

    Design: 토양수분센서 10px -> 1rem 기준으로 변경

--- src/pages/info/SoilInfo.jsx
+++ src/pages/info/SoilInfo.jsx
@@ -4,7 +4,7 @@ import SensorMenu from '../../components/info/SensorMenu';
 import SensorStatus from '../../components/info/SensorStatus';
 import SensorOnOff from '../../components/info/SensorOnOff';
 
-function SoilInfo() {
+export default function SoilInfo() {
   const deviceName = '상추';
   return (
     <>
@@ -19,36 +19,35 @@ function SoilInfo() {
     </>
   );
 }
+
 const Wrapper = styled.div`
+  position: relative;
   width: 90vw;
   height: 85vh;
-  border-radius: 0.5rem;
+  border-radius: 1rem;
   margin: 0 auto;
 
   background: #f0e7e2;
 `;
 
 const DeviceName = styled.p`
-  position: relative;
+  position: absolute;
 
   width: 100%;
-  padding-top: 2%;
-  margin-left: 3%;
-
-  font-family: 'Jua';
-  font-size: 1.5rem;
-  line-height: 1.875rem;
+  margin: 1.7rem 0 0 2.6rem;
 
   color: #c6a692;
+
+  font-family: 'Jua';
+  font-size: 2.4rem;
+  line-height: 3rem;
 `;
 
 const SensorInfoWrapper = styled.div`
   position: absolute;
 
   display: flex;
-  width: 87%;
-  height: 12%;
-  margin-top: 1.5%;
+  width: calc(100% - 4.8rem);
+  height: 10rem;
+  margin: 5.6rem 2.4rem;
 `;
-
-export default SoilInfo;
