16929d9cbefc4fed768e0eedde5f920c3ff20d24
yujin-kwak <yujin02371@gmail.com>
2023-05-10 16:23:02 +0900

    Feat: 온습도 페이지 API 연동

--- src/pages/info/DHTInfo.jsx
+++ src/pages/info/DHTInfo.jsx
@@ -1,24 +1,58 @@
 import React, { useState, useEffect } from 'react';
-// import styled from '@emotion/styled';
-// import SensorMenu from '../../components/info/SensorMenu';
-// import SensorStatus from '../../components/info/SensorStatus';
-// import SensorInfo from '../../components/info/SensorInfo';
+import axios from 'axios';
 import SensorInfoTemplate from '../../components/info/SensorInfoTemplate';
 
 export default function DHTInfo() {
-  const [temp, setTemp] = useState(26);
-  const [humid, setHumid] = useState(70);
+  const [temp, setTemp] = useState(0);
+  const [humid, setHumid] = useState(0);
 
   const deviceName = '상추';
 
   useEffect(() => {
-    const test = setInterval(() => {
-      setTemp(Math.floor(Math.random() * 100));
-      setHumid(Math.floor(Math.random() * 100));
-    }, 2000);
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://34.64.88.23/api/devices/dht', {
+          params: {
+            deviceId: 1
+          }
+        });
 
-    return () => clearInterval(test);
-  }, [temp, humid]);
+        console.log('dataArr', response.data.data);
+        console.log('data', response.data.data[response.data.data.length - 1]);
+
+        const currentData = response.data.data[response.data.data.length - 1];
+        setTemp(currentData.temperature);
+        setHumid(currentData.humidity);
+      } catch (error) {
+        Error('온습도 값을 받아오지 못했습니다.');
+      }
+    };
+
+    fetchData();
+
+    const intervalData = setInterval(async () => {
+      try {
+        const response = await axios.get('http://34.64.88.23/api/devices/dht', {
+          params: {
+            deviceId: 1
+          }
+        });
+
+        console.log('dataArr1', response.data.data);
+        console.log('data1', response.data.data[response.data.data.length - 1]);
+
+        const currentData = response.data.data[response.data.data.length - 1];
+        setTemp(currentData.temperature);
+        setHumid(currentData.humidity);
+      } catch (error) {
+        Error('온습도 값을 받아오지 못했습니다.');
+      }
+    }, 5000);
+
+    return () => {
+      clearInterval(intervalData);
+    };
+  }, []);
 
   return <SensorInfoTemplate deviceName={deviceName} sensorName="온습도" unit="º" sensorData={[temp, humid]} />;
 }
